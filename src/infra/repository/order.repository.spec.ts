d5b9e04ecfaa5b92c2b1870b8fb8625f9a8e97d4
Ruan Ferraz <ruanlima155@gmail.com>
2024-09-04 13:36:36 -0300

    test: order update method

--- src/infra/repository/order.repository.spec.ts
+++ src/infra/repository/order.repository.spec.ts
@@ -99,10 +99,7 @@ describe("Order repository test", () => {
     const orderRepository = new OrderRepository();
     await orderRepository.create(order);
 
-    console.log("order", order);
-
     const foundOrder = await orderRepository.find(order.id);
-    console.log("foundOrder", foundOrder);
 
     expect(foundOrder).toEqual(order);
   });
@@ -149,5 +146,64 @@ describe("Order repository test", () => {
     expect(orders).toContainEqual(order);
     expect(orders).toContainEqual(order2);
     expect(orders).toEqual([order, order2]);
+  });
+
+  it("should update an order", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    const product2 = new Product("1234", "Product 4", 20);
+    await productRepository.create(product);
+    await productRepository.create(product2);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.id,
+      product.name,
+      product.price,
+      2
+    );
+    const orderItem2 = new OrderItem(
+      "2",
+      product2.id,
+      product2.name,
+      product2.price,
+      2
+    );
+    const product3 = new Product("12345", "Product 3", 50);
+    await productRepository.create(product3);
+    const orderItem3 = new OrderItem(
+      "3",
+      product3.id,
+      product3.name,
+      product3.price,
+      2
+    );
+    const product4 = new Product("123456", "Product 4", 50);
+    await productRepository.create(product4);
+    const orderItem4 = new OrderItem(
+      "4",
+      product3.id,
+      product3.name,
+      product3.price,
+      1
+    );
+
+    const order = new Order("123", "123", [orderItem]);
+    const order2 = new Order("1234", "1234", [orderItem2, orderItem4]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+    await orderRepository.create(order2);
+
+    order.addItem(orderItem3);
+    await orderRepository.update(order);
+
+    expect(order.total()).toBe(120);
+    expect(order.items).toHaveLength(2);
+
+    order2.removeItem("4");
+    await orderRepository.update(order2);
+    expect(order2.total()).toBe(40);
+    expect(order2.items).toHaveLength(1);
   });
 });
