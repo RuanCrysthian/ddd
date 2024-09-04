eea9d992e65e0a7339764b327d8681aa6a1d8ef8
Ruan Ferraz <ruanlima155@gmail.com>
2024-09-04 12:34:23 -0300

    feat(repository): add find order by id

--- src/infra/repository/order.repository.ts
+++ src/infra/repository/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order_item";
+import OrderRepositoryInterface from "../../domain/repository/order-repository-interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -21,5 +23,41 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async find(id: string): Promise<Order> {
+    let orderModel;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        include: [{ model: OrderItemModel }],
+        rejectOnEmpty: true,
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    const items = orderModel.items.map(
+      (item) =>
+        new OrderItem(
+          item.id,
+          item.product_id,
+          item.name,
+          item.price,
+          item.quantity
+        )
+    );
+
+    return new Order(id, orderModel.customer_id, items);
+  }
+
+  async findAll(): Promise<Order[]> {
+    throw new Error("Method not implemented.");
   }
 }
